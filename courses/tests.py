011874360e327fdba0852d2437d2b4a78c9c8abd
Alireza Tavakkoli Garmase <alirezatavakkoligarmase@gmail.com>
2021-12-21 17:07:57 +0330

    comment for function in courses application
    insert comment for the test function by me to increase test perception

--- courses/tests.py
+++ courses/tests.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class CourseTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-
+        ### create test database by this function to create courses
         User.objects.create_user(username="mrtook", email="mrtook@gmail.com", first_name="mrtook",
                                  last_name="mrtook",
                                  password="root").save()
@@ -15,7 +15,7 @@ class CourseTest(TestCase):
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vulputate sapien nec sagittis aliquam malesuada bibendum arcu. Lacus laoreet non curabitur gravida. Dis parturient montes nascetur ridiculus mus mauris vitae ultricies leo. Amet cursus sit amet dictum sit amet justo donec enim. Viverra justo nec ultrices dui sapien eget. Dictum varius duis at consectetur. Ac auctor augue mauris augue neque gravida in fermentum. Eleifend donec pretium vulputate sapien nec sagittis. Volutpat maecenas volutpat blandit aliquam. Vestibulum rhoncus est pellentesque elit ullamcorper. Dolor sed viverra ipsum nunc. Sit amet risus nullam eget. Fames ac turpis egestas sed tempus.
 
             Auctor elit sed vulputate mi sit amet mauris commodo. Non nisi est sit amet facilisis magna etiam tempor orci. Libero justo laoreet sit amet. Lobortis scelerisque fermentum dui faucibus in. Vitae suscipit tellus mauris a diam maecenas sed. Bibendum neque egestas congue quisque egestas. Cras fermentum odio eu feugiat pretium nibh ipsum consequat nisl. Facilisi nullam vehicula ipsum a arcu. A lacus vestibulum sed arcu non. Sit amet purus gravida quis blandit. In metus vulputate eu scelerisque felis imperdiet proin fermentum. A arcu cursus vitae congue mauris. Etiam non quam lacus suspendisse faucibus interdum posuere. Volutpat est velit egestas dui id.
-        """
+        """ ### lorem ipsum for overview of course object
         for i in range(5):
             Course.objects.create(owner=User.objects.get(username="mrtook"), title=f"title{i}", price=f"200{i}",
                                   image="courses/None-آموزش_پایتون_از_صفر_تا_صد.png", slug=f"title-{i}",
@@ -25,6 +25,7 @@ class CourseTest(TestCase):
         #print([f"title = {i.title}"+" "+f"price = {i.price}"+" "+f"slug = {i.slug}" for i in cls.all_course_information])
 
     def test_course_page(self):
+        ### this function check the course list page that is loaded or not
         c = Client()
         if c.get("/courses").status_code == 200:
             # print("Course page is loaded successfully")
@@ -34,14 +35,8 @@ class CourseTest(TestCase):
         pass
 
     def test_details_course_page(self):
+        ### this test check course details that i can get all information about a course from database correct or not
         c = Client()
         for i in self.all_course_information:
             if c.get(f"/course/{i.id}/{i.slug}").status_code == 200:
                 print(f"course's {i.title} is available ok 200")
-
-            #print(c.get(f"http://127.0.0.1:8000/course/{i.id}/{i.slug}"))
-
-            # if i.overview in c.get(f"/course/{i.id}/{i.slug}").content:
-            #     self.assertTrue(True, msg=f"{i.title} there is a right content ok 200")
-            # else:
-            #     self.assertTrue(False, msg=f"{i.title} there isn't a right content")
