21d3092c0f1f0b01601b307cc83a9361169f78b4
Alireza Tavakkoli Garmase <alirezatavakkoligarmase@gmail.com>
2021-12-21 09:48:13 +0330

    update newsletter section in all templates and created prototype test case to test course application

--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase,Client
 
-# Create your tests here.
+
+class CourseTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        pass
+
+    def test_course_page(self):
+        pass
+    def test_search_course(self):
+        pass
+    def test_details_course_page(self):
+        pass
+
+
